2eb04c74f68d195a32d46ea81f2945e343ca730c
cct1225 <grace951225@gmail.com>
2023-09-02 23:57:05 +0800

    test

--- SQL/stack.cpp
+++ SQL/stack.cpp
@@ -2,7 +2,7 @@
 #include<stack>
 using namespace std;
 int main(){
-    /*stack<int> mystack;
+    stack<int> mystack;
     mystack.push(1);//     1
     mystack.push(2);//   2 1
     mystack.push(3);// 3 2 1
@@ -11,18 +11,7 @@ int main(){
     {
         cout<<mystack.top();
         mystack.pop();
-    }*/
-    
-    stack<int> mystack;
-    // std::cout << mystack.top() << "\n"; // Segmentation fault
-    if (!mystack.empty())
-        std::cout << mystack.top() << "\n";
-    mystack.push(1);
-    mystack.push(2);
-    mystack.push(3);
-    if (!mystack.empty())
-        std::cout << mystack.top() << "\n"; // 3
-
+    }
 
 
 
--- SQL/stack_copiedfrom_vector.cpp
+++ SQL/stack_copiedfrom_vector.cpp
@@ -1,4 +1,3 @@
-/*
 #include <iostream>
 #include<vector>
 #include<stack>
@@ -10,21 +9,6 @@ int main() {
     {
         cout<<mystack2.top()<<" ";//21
         mystack2.pop(); 
-    }
-    return 0;
-}*/
-// Online C++ compiler to run C++ program online
-#include <iostream>
-#include<vector>
-#include<queue>
-using namespace std;
-int main() {
-    vector<int> myvector = {1, 2};
-    queue<int, vector<int>> myqueue(myvector);
-    while(!myqueue.empty())
-    {
-        cout<<myqueue.front()<<" ";
-        myqueue.pop(); 
     }
     return 0;
 }
